5e1ae61fe3f8cb742ec2b4a87f5ac374e418bd51
fikrimhammd <fikri.kdi60@gmail.com>
2025-12-19 20:11:08 +0700

    membuat RepositoryDataSiswa.kt

---
+++ app/src/main/java/com/example/mysql/repository/RepositoryDataSiswa.kt
@@ -0,0 +1,18 @@
+package com.example.mysql.repository
+
+import com.example.mysql.apiservice.ServiceApiSiswa
+import com.example.mysql.modeldata.DataSiswa
+
+
+interface RepositoryDataSiswa {
+    suspend fun getDataSiswa(): List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void>
+}
+
+class jaringanRepositoryDataSiswa (
+    private val serviceApiSiswa: ServiceApiSiswa
+): RepositoryDataSiswa{
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2
+    .Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+}
