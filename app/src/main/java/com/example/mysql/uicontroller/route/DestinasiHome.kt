51a601028d151731b73422190938817a193bad63
fikrimhammd <fikri.kdi60@gmail.com>
2025-12-19 20:06:47 +0700

    membuat DestinasiHome.kt.kt

---
+++ app/src/main/java/com/example/mysql/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.mysql.uicontroller.route
+
+import com.example.mysql.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.nama
+}
