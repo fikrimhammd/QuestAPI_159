95e36e5a8b9e67e38684aebbe278c160487b0b6e
fikrimhammd <fikri.kdi60@gmail.com>
2025-12-19 20:10:31 +0700

    membuat PenyediaViewModel.kt

---
+++ app/src/main/java/com/example/mysql/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.mysql.viewmodel.provider
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.mysql.repository.AplikasiDataSiswa
+import com.example.mysql.viewmodel.EntryViewModel
+import com.example.mysql.viewmodel.HomeViewModel
+
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
+            AplikasiDataSiswa
+            )
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(
+                aplikasiDataSiswa().container
+                    .repositoryDataSiswa
+            )
+        }
+        initializer {
+            EntryViewModel(
+                aplikasiDataSiswa().container
+                    .repositoryDataSiswa
+            )
+        }
+    }
+}
